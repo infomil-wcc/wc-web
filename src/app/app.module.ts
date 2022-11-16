6ba49205d530d6645a5289dfaba2ea0831d30bc0
infomil.worldcup@gmail.com <olivier.infomil2019@gmail.com>
2022-11-16 11:21:57 +0400

    init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TeamComponent } from './components/team/team.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { TopHeaderComponent } from './components/top-header/top-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TeamComponent,
+    NavigationComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
