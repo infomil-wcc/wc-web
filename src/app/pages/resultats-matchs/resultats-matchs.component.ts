9bfbe3e93d61f26e2bc7bef10e88ba332a355ff2
Olivier Lebrasse <olivier@omediainteractive.net>
2022-11-19 14:02:34 +0400

    updates to matches

--- src/app/pages/resultats-matchs/resultats-matchs.component.ts
+++ src/app/pages/resultats-matchs/resultats-matchs.component.ts
@@ -10,7 +10,7 @@ export class ResultatsMatchsComponent implements OnInit {
 
   constructor(private matchService : MatchResultsService) { }
 
-  public resultats: any = [];
+  public match: any = [];
   public loaded:boolean = false;
   public uniqueDates: any = [];
 
@@ -37,7 +37,7 @@ export class ResultatsMatchsComponent implements OnInit {
             "winDraw": elem.winner_draw,
           }
 
-          this.resultats.push(res);
+          this.match.push(res);
 
           if(count == dataLength){
             this.loaded = true;
